4ca510b3a7ac68ef43765e84a089182a0b33dccf
Bastian Bittorf <bb@npl.de>
2021-01-13 19:47:25 +0100

    travis: use ci_helper

--- build_qemu_image.sh
+++ build_qemu_image.sh
@@ -1,28 +1,9 @@
 #!/bin/sh
 
-MES_PWD="$( pwd )"		# original repo
-
-TMPDIR="$( mktemp -d )" || exit
-cd "$TMPDIR" || exit
-
 git clone --depth 1 https://github.com/bittorf/kritis-linux.git
-cd * || exit
-
-TMP1="$( mktemp )" || exit
-TMP2="$( mktemp )" || exit
-
-KEEP='/bin/busybox /bin/sh /bin/cat'
-echo "[OK] INITRD_DIR_ADD='$MES_PWD' KEEP_LIST='$KEEP' ./minilinux.sh latest"
-
-INITRD_DIR_ADD="$MES_PWD" KEEP_LIST="$KEEP" ./minilinux.sh latest >"$TMP1" 2>"$TMP2" || {
-	RC="$?"
-	cat "$TMP1" "$TMP2"
-	exit "$RC"
-}
-
-ls -l minilinux/builds/linux/arch/x86/boot/bzImage	# kernel.bin
-ls -l minilinux/builds/initramfs.cpio.xz.xz 		# initrd.xz
-
-echo "[OK] now running 'minilinux/builds/linux/run.sh' in autotest-mode"
-minilinux/builds/linux/run.sh autotest "In QEMU-mode you can now explore the system" 120
-
+kritis-linux/ci_helper.sh \
+	--keep '/bin/busybox /bin/sh /bin/cat' \
+	--diradd "$( pwd )" \
+	--pattern "In QEMU-mode you can now explore the system" \
+	--maxwait "600" \
+	--linux "latest"
