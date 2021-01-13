0767c99cb7c31425481dd5d4cefdf5d73f75556e
Bastian Bittorf <bb@npl.de>
2021-01-13 09:21:47 +0100

    build_qemu_image.sh: try to build

--- build_qemu_image.sh
+++ build_qemu_image.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-true
+MES_PWD="$( pwd )"		# original repo
+
+TMPDIR="$( mktemp -d )" || exit
+cd "$TMPDIR" || exit
+
+git clone https://github.com/bittorf/kritis-linux.git
+cd * || exit
+
+KEEP='/bin/busybox /bin/sh /bin/cat'
+INITRD_DIR_ADD="$MES_PWD" KEEP_LIST="$KEEP" ./minilinux.sh latest || exit
+
+ls -l builds/linux/arch/x86/boot/bzImage	# kernel.bin
+ls -l builds/initramfs.cpio.xz.xz 		# initrd.xz
