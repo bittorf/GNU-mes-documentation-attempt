9d376df5d4ff9d443a6efa56d1f7ddf4f7e32c0a
Bastian Bittorf <bb@npl.de>
2021-01-13 09:57:17 +0100

    build_qemu_image.sh: more debug

--- build_qemu_image.sh
+++ build_qemu_image.sh
@@ -8,8 +8,18 @@ cd "$TMPDIR" || exit
 git clone https://github.com/bittorf/kritis-linux.git
 cd * || exit
 
+TMP1="$( mktemp )" || exit
+TMP2="$( mktemp )" || exit
+
 KEEP='/bin/busybox /bin/sh /bin/cat'
-INITRD_DIR_ADD="$MES_PWD" KEEP_LIST="$KEEP" ./minilinux.sh latest || exit
+echo "[OK] INITRD_DIR_ADD='$MES_PWD' KEEP_LIST='$KEEP' ./minilinux.sh latest"
+
+INITRD_DIR_ADD="$MES_PWD" KEEP_LIST="$KEEP" ./minilinux.sh latest >"$TMP1" 2>"$TMP2" || {
+	RC="$?"
+	cat "$TMP1" "$TMP2"
+	apt-cache search qemu
+	exit "$RC"
+}
 
 ls -l minilinux/builds/linux/arch/x86/boot/bzImage	# kernel.bin
 ls -l minilinux/builds/initramfs.cpio.xz.xz 		# initrd.xz
