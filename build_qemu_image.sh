3601f27703594415b699833e79a82060cf774759
Bastian Bittorf <bb@npl.de>
2021-01-14 08:42:06 +0100

    travis: lowering max execution time to 200 sec

--- build_qemu_image.sh
+++ build_qemu_image.sh
@@ -7,7 +7,7 @@ kritis-linux/ci_helper.sh \
 	--keep '/bin/busybox /bin/sh /bin/cat' \
 	--diradd "$( pwd )" \
 	--pattern "In QEMU-mode you can now explore the system" \
-	--maxwait "600" \
+	--maxwait "200" \
 	--kernel "latest" \
 	--init "run-amd64.sh" \
 	--core 'busybox'
